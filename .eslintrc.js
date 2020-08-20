88cc649c49f0c76e0bca5d3a64c3899864206eb5
Carlos Perez <carlos.perez@charliegperez.com>
2020-08-20 01:09:05 -0500

    installation and use of eslint, prettier and husky

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error',
+};
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-console': RULES.WARN,
+    'jsx-quotes': [RULES.ERROR, 'prefer-single'],
+  },
+};
