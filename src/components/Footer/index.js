3a115fa27a4b33c43e34b3c89398a30b7b4cbba3
Carlos Perez <carlos.perez@charliegperez.com>
2020-08-22 19:55:23 -0500

    adding Layout, Header and footer components, and style corresponding

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../sass/footer.scss';
+
+const index = () => (
+  <footer>
+    <p>Derechos Reservados | Términos y Condiciones | Aviso de Privacidad</p>
+  </footer>
+);
+
+export default index;
---
+++ src/components/Header/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+import '../../sass/header.scss';
+import '../../sass/menu.scss';
+
+const Index = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  return (
+    <header className='header'>
+      <img className='header__logo' src={logo} alt='Babysitter Finder' />
+      <span
+        className='header--hamburger material-icons'
+        onClick={() => setOpenMenu(!openMenu)}
+      >
+        menu
+      </span>
+      <nav className={!openMenu ? 'navigation' : 'navigation navigation-open'}>
+        <div className='navigation__profile'>
+          <span className='material-icons navigation--icon'>
+            account_circle
+          </span>
+          <Link to='/'> Nombre de usuario </Link>
+          <Link to='/'>Cerrar Sesión</Link>
+        </div>
+        <ul className='navigation__ul'>
+          <li className='navigation__li'>
+            <Link className='navigation__link' to='/'>
+              Loguin
+            </Link>
+          </li>
+          <li className='navigation__li'>
+            <Link className='navigation__link' to='/'>
+              Contact
+            </Link>
+          </li>
+          <li className='navigation__li'>
+            <Link className='navigation__link' to='/'>
+              Item 1
+            </Link>
+          </li>
+          <li className='navigation__li'>
+            <Link className='navigation__link' to='/'>
+              Item 2
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Index;
