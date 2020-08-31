0cd39b0f2880dff8db08b795c1bdc7de57d6d18f
Carlos Perez <carlos.perez@charliegperez.com>
2020-08-30 22:31:38 -0500

    creación de la funcionalidad del login con Auth0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import NanaProfile from '../src/pages/NanaProfile';
 import Filter from '../src/pages/Filter';
 import { ServerProvider } from '../src/components/Contex/Server';
 import Home from './pages/Home';
 import Layout from './components/Layout';
 import NotFound from './pages/NotFound';
+import PrivateRoute from './components/Login/PrivateRoute';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  console.log('useAuth0 rutas: ', useAuth0());
+  let role = [];
+  const objectrole = useAuth0().user;
+  console.log(objectrole);
+  if (objectrole) {
+    console.log('role informado');
+    role = Object.values(objectrole);
+    console.log('role', role[0][0]);
+    const rolenuevo = role[0][0];
+    localStorage.setItem('role', rolenuevo);
+  }
+  console.log('localStorage', localStorage.getItem('role'));
   return (
     <div className='App'>
       <ServerProvider>
-        <Router>
-          <Layout>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/nana' component={Filter} />
-              <Route exact path='/nana/:id' component={NanaProfile} />
-              <Route component={NotFound} />
-            </Switch>
-          </Layout>
-        </Router>
+        <Layout>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <PrivateRoute exact path='/nana' component={Filter} />
+            <PrivateRoute exact path='/nana/:id' component={NanaProfile} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
       </ServerProvider>
     </div>
   );
---
+++ src/components/Login/PrivateRoute/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/display-name */
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+import Loading from '../../Loading';
+
+const index = ({ component, ...args }) => (
+  <Route
+    component={withAuthenticationRequired(component, {
+      onRedirecting: () => <Loading />,
+    })}
+    {...args}
+  />
+);
+
+export default index;
